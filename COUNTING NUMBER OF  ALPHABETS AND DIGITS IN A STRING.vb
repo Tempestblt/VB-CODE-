cbf38a80832869236f1657c6a4e825bee62a67f1
Hassaan Fazli <118451468+Tempestblt@users.noreply.github.com>
2022-11-24 21:42:37 +0500

    Add files via upload

---
+++ COUNTING NUMBER OF  ALPHABETS AND DIGITS IN A STRING.vb
@@ -0,0 +1,39 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim stri1, number As String
+        Dim i, alphacount, Numbercount As Integer
+
+        Dim character As Char
+
+
+
+        Console.Write("ENTER STRING: ")
+        stri1 = Console.ReadLine
+
+
+
+
+        For i = 1 To Len(stri1)
+            character = Mid(stri1, i, 1)
+            If InStr(character, "qwertyuiopasdfghjklzxcvbnm") > 0 Then
+                alphacount = alphacount + 1
+            End If
+
+        Next
+
+        For i = 1 To Len(stri1)
+            number = Mid(stri1 , i , 1)
+            If InStr(number, "123456789") > 0 Then
+                Numbercount = Numbercount + 1
+
+            End If
+
+        Next
+        Console.WriteLine(" THIS IS THE NUMBER OF WORDS IN YOUR STRING " & alphacount)
+        Console.WriteLine(" THIS IS THE NUMBER OF NUMBERS IN YOUR STRING " & Numbercount)
+        Console.ReadKey()
+
+    End Sub
+
+End Module
