eb6333ec84e458e94dd7bed10db7f70254721d2e
Hassaan Fazli <118451468+Tempestblt@users.noreply.github.com>
2022-11-24 22:18:01 +0500

    Add files via upload

---
+++ ABBREVIATING A STRING.vb
@@ -0,0 +1,29 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim stri1 As String
+        Dim space, secspace As Integer
+        Dim FW, SW, TW As Char
+
+
+
+
+
+        Console.Write("PLEASE ENTER THE STRING TO BE ABBREVIATED: ")
+        stri1 = Console.ReadLine()
+
+        FW = Left(stri1, 1)
+
+        space = InStr(stri1, " ")
+        SW = Mid(stri1, space + 1, 1)
+
+        secspace = InStr(space + 1, stri1, " ")
+        TW = Mid(stri1, secspace + 1, 1)
+
+        Console.Write(" THE ABBREVIATED VERSION IS: " & FW & SW & TW)
+
+        Console.ReadKey()
+
+    End Sub
+
+End Module
