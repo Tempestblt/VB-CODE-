10cd1b3e0c3af0405440370758936a4f29b03cd7
Hassaan Fazli <118451468+Tempestblt@users.noreply.github.com>
2022-11-25 11:02:01 +0500

    Add files via upload

--- ABBREVIATING A STRING.vb
+++ ABBREVIATING A STRING.vb
@@ -1,7 +1,7 @@
 ﻿Module Module1
 
     Sub Main()
-        Dim stri1 As String
+        Dim stri1, stri2 As String
         Dim space, numbofwords, secspace As Integer
         Dim FW, SW, TW As Char
 
@@ -14,26 +14,26 @@
         Console.Write("PLEASE ENTER THE NUMBER OF WORDS IN YOUR STRING: ")
         numbofwords = Console.ReadLine
 
-
+        stri2 = UCase(stri1)
         If numbofwords = 1 Then
-            FW = Left(stri1, 1)
+            FW = Left(stri2, 1)
 
         ElseIf numbofwords = 2 Then
-            FW = Left(stri1, 1)
+            FW = Left(stri2, 1)
 
-            space = InStr(stri1, " ")
-            SW = Mid(stri1, space + 1, 1)
+            space = InStr(stri2, " ")
+            SW = Mid(stri2, space + 1, 1)
 
         ElseIf numbofwords = 3 Then
-            FW = Left(stri1, 1)
+            FW = Left(stri2, 1)
 
-            space = InStr(stri1, " ")
-            SW = Mid(stri1, space + 1, 1)
+            space = InStr(stri2, " ")
+            SW = Mid(stri2, space + 1, 1)
 
-            secspace = InStr(space + 1, stri1, " ")
-            TW = Mid(stri1, secspace + 1, 1)
+            secspace = InStr(space + 1, stri2, " ")
+            TW = Mid(stri2, secspace + 1, 1)
         End If
-        
+
 
         Console.Write(" THE ABBREVIATED VERSION IS: " & FW & SW & TW)
 
