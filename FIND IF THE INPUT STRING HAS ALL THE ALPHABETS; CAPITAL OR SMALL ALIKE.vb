01e089b2279159fbe4ba2ac24cd79f3512cac0d7
Hassaan Fazli <118451468+Tempestblt@users.noreply.github.com>
2022-11-25 18:20:38 +0500

    Add files via upload

---
+++ FIND IF THE INPUT STRING HAS ALL THE ALPHABETS; CAPITAL OR SMALL ALIKE.vb
@@ -0,0 +1,35 @@
+﻿Module Module1
+
+    Sub Main()
+        'Find if the input string has all the alphabets; capital or small alike.
+
+
+        Dim string1 As String
+        Dim A As Char
+        Dim i, COUNT As Integer
+
+
+        Console.Write("PLEASE ENTER STRING: ")
+        string1 = Console.ReadLine
+
+        For i = 1 To Len(string1)
+            A = Mid(string1, i, 1)
+            If InStr("QWERTYUIOPASDFGHJKLZXCVBNM", A) > 0 Then
+                COUNT = COUNT + 1
+
+            ElseIf InStr("QWERTYUIOPASDFGHJKLZXCVBNM", A) = 0 Then
+                COUNT = COUNT + 0
+
+            End If
+
+        Next
+        If COUNT > 0 Then
+            Console.WriteLine("ALL THE ALPHABETS ARE IN CAPITAL")
+        ElseIf COUNT = 0 Then
+            Console.WriteLine("ALL THE ALPHABETS ARE  SMALL")
+        End If
+      
+        Console.ReadKey()
+    End Sub
+
+End Module
