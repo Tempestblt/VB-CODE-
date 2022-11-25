d108cfbefb452f913bf277731d2a4093e41e9f5b
Hassaan Fazli <118451468+Tempestblt@users.noreply.github.com>
2022-11-25 11:51:57 +0500

    Add files via upload

--- COUNTING NUMBER OF  ALPHABETS AND DIGITS IN A STRING.vb
+++ COUNTING NUMBER OF  ALPHABETS AND DIGITS IN A STRING.vb
@@ -1,10 +1,10 @@
 ﻿Module Module1
 
     Sub Main()
-        Dim stri1, stri2, D As String
+        Dim stri1, stri2 As String
         Dim i, alphacount, A, B, numbercount As Integer
 
-        Dim E As Char
+        Dim E, D As Char
 
 
 
@@ -16,7 +16,7 @@
         stri2 = LCase(stri1)
         For i = 1 To Len(stri2)
             E = Mid(stri2, i, 1)
-            A = InStr(E, "qwertyuiopasdfghjklzxcvbnm")
+            A = InStr("qwertyuiopasdfghjklzxcvbnm", E)
             If A <> 0 Then
                 alphacount = alphacount + 1
             End If
@@ -25,7 +25,7 @@
 
         For i = 1 To Len(stri2)
             D = Mid(stri2, i, 1)
-            B = InStr(D, "123456789")
+            B = InStr("123456789", D)
             If B > 0 Then
                 numbercount = numbercount + 1
 
@@ -33,7 +33,7 @@
 
         Next
         Console.WriteLine(" THIS IS THE NUMBER OF WORDS IN YOUR STRING " & alphacount)
-        Console.WriteLine(" THIS IS THE NUMBER OF NUMBERS IN YOUR STRING " & Numbercount)
+        Console.WriteLine(" THIS IS THE NUMBER OF NUMBERS IN YOUR STRING " & numbercount)
         Console.ReadKey()
 
     End Sub
