0de16e6e857c045d1ef7f27fe4c5a1ad18e2faf4
Hassaan Fazli <118451468+Tempestblt@users.noreply.github.com>
2022-11-27 17:04:35 +0500

    Add files via upload

---
+++ INPUT A STRING S, AND OUTPUT A STRING MADE OF THE FIRST 2 AND THE LAST 2 CHARS.vb
@@ -0,0 +1,41 @@
+﻿Module Module1
+
+    Sub Main()
+        'INPUT A STRING S, AND OUTPUT A STRING MADE OF THE FIRST 2 AND THE LAST 2 CHARS
+
+
+        Dim Str1, Str2, STRI4 As String
+        Dim Char1, A, B, C, D As Char
+        Dim i As Integer
+
+
+        Str1 = ""
+        Str2 = ""
+        Char1 = ""
+        i = 0
+
+       
+
+        Console.Write("Enter string to process: ")
+        Str1 = Console.ReadLine
+
+        A = Mid(Str1, 1, 1)
+        B = Mid(Str1, 2, 1)
+
+
+        For i = Len(Str1) To 1 Step -1
+            Char1 = Mid(Str1, i, 1)
+            Str2 = Str2 & Char1
+        Next
+
+        D = Mid(Str2, 1, 1)
+        C = Mid(Str2, 2, 1)
+
+        STRI4 = A & B & C & D
+
+        Console.Write("THE RESULT IS: " & STRI4)
+        Console.ReadKey()
+
+    End Sub
+
+End Module
