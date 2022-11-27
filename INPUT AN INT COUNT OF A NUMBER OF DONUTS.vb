42e19cd83668d4c206f6742adba0bde577bfe497
Hassaan Fazli <118451468+Tempestblt@users.noreply.github.com>
2022-11-27 14:49:08 +0500

    Add files via upload

---
+++ INPUT AN INT COUNT OF A NUMBER OF DONUTS.vb
@@ -0,0 +1,28 @@
+﻿Module Module1
+
+    Sub Main()
+        'INPUT AN INT COUNT OF A NUMBER OF DONUTS
+
+        Dim i As Integer
+
+        Console.Write("PLEASE ENTER YOUR DONUTS COUNT: ")
+        i = Console.ReadLine
+
+
+
+            If i >= 10 Then
+                Console.WriteLine("NUMBER OF DONUTS: MANY")
+
+            ElseIf i < 10 Then
+                Console.WriteLine("NUMBER OF DONUTS: " & i)
+
+
+            End If
+
+            Console.ReadKey()
+
+
+
+    End Sub
+
+End Module
